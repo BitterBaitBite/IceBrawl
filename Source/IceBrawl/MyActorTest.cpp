48b5546754a5ed6adff2a23e06f77ff4435cc60c
BitterBaitBite <guillechernandez@gmail.com>
2025-03-06 22:33:22 +0100

    Base

---
+++ Source/IceBrawl/MyActorTest.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActorTest.h"
+
+AMyActorTest::AMyActorTest()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActorTest::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AMyActorTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+}
+
