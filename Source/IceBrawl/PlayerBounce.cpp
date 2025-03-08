3437a864dd8faac14c9c90abe17c4e89bfd7daab
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2025-03-08 22:04:00 +0100

    Segundo jugador y físicas de rebote

---
+++ Source/IceBrawl/PlayerBounce.cpp
@@ -0,0 +1,75 @@
+#include "PlayerBounce.h"
+#include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
+#include "GameFramework/Character.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UPlayerBounce::UPlayerBounce()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UPlayerBounce::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+// Called every frame
+void UPlayerBounce::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UPlayerBounce::HandleBounce(AActor* OtherActor, const FVector& HitNormal, const FVector& MyVelocity, const FVector& OtherVelocity)
+{
+    if (OtherActor)
+    {
+        // Get velocities of both objects
+        FVector MyCurrentVelocity = MyVelocity;
+        FVector OtherCurrentVelocity = OtherVelocity;
+
+        // Get the mass of both objects (you may want to actually fetch these values dynamically)
+        float MyMass = 50.f;
+        float OtherMass = 50.f;
+
+        // Calculate relative velocity
+        FVector RelativeVelocity = MyCurrentVelocity - OtherCurrentVelocity;
+
+        // Compute the impulse magnitude based on the collision
+        float ImpulseMagnitude = FVector::DotProduct(RelativeVelocity, HitNormal) * (1 + Restitution);
+
+        // Compute the reflected velocities using the reflection formula
+        FVector MyReflectiveVelocity = MyCurrentVelocity - (2 * ImpulseMagnitude * HitNormal) * (OtherMass / (MyMass + OtherMass));
+        FVector OtherReflectiveVelocity = OtherCurrentVelocity + (2 * ImpulseMagnitude * HitNormal) * (MyMass / (MyMass + OtherMass));
+
+        // Apply the new velocity using LaunchCharacter
+        ACharacter* MyCharacter = Cast<ACharacter>(GetOwner());
+        if (MyCharacter)
+        {
+            // No need for the minus sign here
+            FVector FinalLaunchVelocity = MyReflectiveVelocity;
+            MyCharacter->LaunchCharacter(FinalLaunchVelocity, true, true);
+
+            // Debugging
+            FString VectorString = FinalLaunchVelocity.ToString();
+            FString NormalString = HitNormal.ToString();
+            GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, "Launch Vel: " + VectorString + " | Normal: " + NormalString);
+        }
+
+        // If you want to launch the other actor (if it's also a character), you could use similar logic
+        ACharacter* OtherCharacter = Cast<ACharacter>(OtherActor);
+        if (OtherCharacter)
+        {
+            // No need for the minus sign here
+            FVector FinalLaunchVelocity = OtherReflectiveVelocity;
+            OtherCharacter->LaunchCharacter(FinalLaunchVelocity, true, true);
+
+            // Debugging
+            FString VectorString = FinalLaunchVelocity.ToString();
+            FString NormalString = HitNormal.ToString();
+            GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, "Launch Vel: " + VectorString + " | Normal: " + NormalString);
+        }
+    }
+}
---
+++ Source/IceBrawl/PlayerBounce.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerBounce.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ICEBRAWL_API UPlayerBounce : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this component's properties
+    UPlayerBounce();
+
+protected:
+    // Called when the game starts
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    // Bounce functionality
+    UFUNCTION(BlueprintCallable)
+    void HandleBounce(AActor* OtherActor, const FVector& HitNormal, const FVector& MyVelocity, const FVector& OtherVelocity);
+
+private:
+    // Coefficient of restitution (bounciness factor)
+    float Restitution = 1.0f; // 1.0 for fully elastic collision, 0 for inelastic
+
+		
+};
